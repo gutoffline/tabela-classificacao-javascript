c1c76fe1f265b17efa45bc9a6d64f623bae749cd
Guto Xavier <gutoffline@gmail.com>
2021-04-08 20:13:56 -0300

    função para exibir jogadores na tela

--- js/funcoes.js
+++ js/funcoes.js
@@ -14,10 +14,32 @@ let julia = {
     pontos: 7
 }
 
+
+
 guilherme.pontos = CalculaPontos(guilherme);
 julia.pontos = CalculaPontos(julia);
 
 function CalculaPontos(jogador){
     let pontos = (jogador.vitorias * 3) + jogador.empates;
     return pontos;
+}
+
+let jogadores = [guilherme , julia];
+ExibirJogadoresNaTela(jogadores);
+
+function ExibirJogadoresNaTela(jogadores){
+    let html = "";
+    for(let i = 0 ; i < jogadores.length; i++){
+        html += "<tr><td>" + jogadores[i].nome + "</td>";
+        html += "<td>" + jogadores[i].vitorias + "</td>";
+        html += "<td>" + jogadores[i].empates + "</td>";
+        html += "<td>" + jogadores[i].derrotas + "</td>";
+        html += "<td><button>Vitória</button></td>";
+        html += "<td><button>Empate</button></td>";
+        html += "<td><button>Derrota</button></td>";
+        html += "</tr>";
+    }
+
+    let tabelaJogadores = document.querySelector("#tabelaJogadores");
+    tabelaJogadores.innerHTML = html;
 }
