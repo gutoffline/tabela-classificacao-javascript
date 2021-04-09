ba768fba326fe884603832d3c4c3377f4023f3ed
Guto Xavier <gutoffline@gmail.com>
2021-04-08 21:22:18 -0300

    adicionar vitórias, empates e derrotas

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,17 +1,17 @@
 let guilherme = {
     nome:"Guilherme",
-    vitorias:2,
-    empates:1,
-    derrotas:1,
-    pontos:7
+    vitorias:0,
+    empates:0,
+    derrotas:0,
+    pontos:0
 }
 
 let julia = {
     nome: "Julia",
-    vitorias: 3,
-    empates: 4,
-    derrotas: 2,
-    pontos: 7
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    pontos: 0
 }
 
 
@@ -34,12 +34,33 @@ function ExibirJogadoresNaTela(jogadores){
         html += "<td>" + jogadores[i].vitorias + "</td>";
         html += "<td>" + jogadores[i].empates + "</td>";
         html += "<td>" + jogadores[i].derrotas + "</td>";
-        html += "<td><button>Vitória</button></td>";
-        html += "<td><button>Empate</button></td>";
-        html += "<td><button>Derrota</button></td>";
+        html += "<td>" + jogadores[i].pontos + "</td>";
+        html += "<td><button onclick='AdicionarVitoria("+ i + ")'>Vitória</button></td>";
+        html += "<td><button onclick='AdicionarEmpate(" + i + ")'>Empate</button></td>";
+        html += "<td><button onclick='AdicionarDerrota("+ i +")'>Derrota</button></td>";
         html += "</tr>";
     }
 
     let tabelaJogadores = document.querySelector("#tabelaJogadores");
     tabelaJogadores.innerHTML = html;
+}
+
+function AdicionarVitoria(i){
+    let jogador = jogadores[i];
+    jogador.vitorias++;
+    jogador.pontos = CalculaPontos(jogador);
+    ExibirJogadoresNaTela(jogadores);
+}
+
+function AdicionarEmpate(i){
+    let jogador = jogadores[i];
+    jogador.empates++;
+    jogador.pontos = CalculaPontos(jogador);
+    ExibirJogadoresNaTela(jogadores);
+}
+
+function AdicionarDerrota(i){
+    let jogador = jogadores[i];
+    jogador.derrotas++;
+    ExibirJogadoresNaTela(jogadores);
 }
